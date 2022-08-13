684faa5dfa118d8057accc2f9b17e171666e7984
Praise KANGNIDE <kangnideblessin@gmail.com>
2022-08-13 21:22:24 +0100

    creation BD

---
+++ database/migrations/2022_08_13_194930_create_evenements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEvenementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evenements', function (Blueprint $table) {
+            $table->id();
+            $table->string("nom");
+            $table->string("date_demarrage");
+            $table->timestamp("date_fin");
+            $table->string("description");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evenements');
+    }
+}
